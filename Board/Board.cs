826eb2fada81cb167051e771130261dde11c3d05
juancoquet <juancoquet@gmail.com>
2023-07-09 11:40:12 +0100

    record state history

--- Board/Board.cs
+++ Board/Board.cs
@@ -10,10 +10,13 @@ public class ChessBoard
     public int MoveNumber    { get; set; } = 1;
     public bool InCheck      { get; set; } = false;
 
+    private List<BoardState> _history { get; set; } = new List<BoardState>();
+
     public ChessBoard()
     {
         BitBoard = new BitBoard();
         Squares = Enumerable.Repeat(Piece.None(), 64).ToArray();
+        RecordState();
     }
 
     private void PlacePiece(Squares sq, Colour colour, PieceType pieceType)
@@ -39,5 +42,26 @@ public class ChessBoard
         }
 
         public static Piece None() => new Piece(Colour.None, PieceType.None);
+    }
+
+    private void RecordState()
+    {
+        _history.Add(new BoardState()
+        {
+            BitBoard = BitBoard,
+            Squares = Squares,
+            Turn = Turn,
+            MoveNumber = MoveNumber,
+            InCheck = InCheck,
+        });
+    }
+
+    internal record BoardState
+    {
+        public required BitBoard BitBoard { get; init; }
+        public required IPiece[] Squares  { get; init; }
+        public required Colour Turn       { get; init; }
+        public required int MoveNumber    { get; init; }
+        public required bool InCheck      { get; init; }
     }
 }
