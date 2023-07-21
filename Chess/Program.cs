1ca3e747fc50a288f9024cde52047d75b0b86fa7
juancoquet <juancoquet@gmail.com>
2023-07-21 23:52:11 +0100

    set up raylib gui window

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -10,4 +10,3 @@ class Program
         engine.Run();
     }
 }
-
--- Gui/Program.cs
+++ Gui/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+
+namespace Gui;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Raylib.InitWindow(800, 600, "Juan's Chess");
+        Raylib.SetTargetFPS(60);
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.DARKGRAY);
+            Raylib.DrawText("Juan's chess engine", 12, 12, 20, Color.WHITE);
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+    }
+}
